17557be160dc16cf9b0161a048a2ee8990badaba
raymatsuura <ray.matsuura.01@gmail.com>
2025-04-12 21:05:10 +0900

    Fix build errors by removing backup files and fixing ESLint config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,2 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  
-  // 最小限の設定
-  images: {
-    domains: ['lh3.googleusercontent.com', 'firebasestorage.googleapis.com'],
-  },
-  
-  // ビルドエラーは無視
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-};
-
+const nextConfig = { reactStrictMode: true };
 module.exports = nextConfig;
